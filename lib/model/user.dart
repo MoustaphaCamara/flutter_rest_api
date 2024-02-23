b271fdeaacb33720f097c542d9740f409b7db042
MoustaphaCamara <cmr.mous@gmail.com>
2024-02-23 15:03:23 +0100

    feat(api): user type before migrating to currency API

---
+++ lib/model/user.dart
@@ -0,0 +1,28 @@
+typedef Json = Map<String, dynamic>;
+
+class User {
+  final String gender;
+  final String email;
+  final String name;
+  final String phone;
+  final String nat;
+
+  /*
+      v2
+      User({
+      required this.gender,
+      required this.email,
+      required this.name,
+      required this.phone,
+      required this.nat,
+      });
+   */
+
+  User.fromJson(Json json)
+      : gender = json['gender'],
+        email = json['email'],
+        name = json['name'],
+        phone = json['phone'],
+        nat = json['nat']
+}
+
